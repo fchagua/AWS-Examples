38d98b45255e444942a048c98b56c900bfea2d06
Francisco Chagua <franciscochagua226@gmail.com>
2025-03-11 19:54:36 +0000

    Provide etag example

---
+++ etags/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("myfile.txt")
+}
